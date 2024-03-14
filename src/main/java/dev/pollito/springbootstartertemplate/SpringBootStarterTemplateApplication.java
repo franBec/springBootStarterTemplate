d8d06e981812abe1e03931d2499d0078502f9141
Fran Bec <franbecvort@gmail.com>
2024-03-14 10:53:48 +0000

    initial commit

---
+++ src/main/java/dev/pollito/springbootstartertemplate/SpringBootStarterTemplateApplication.java
@@ -0,0 +1,12 @@
+package dev.pollito.springbootstartertemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterTemplateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootStarterTemplateApplication.class, args);
+  }
+}
---
+++ src/test/java/dev/pollito/springbootstartertemplate/SpringBootStarterTemplateApplicationTests.java
@@ -0,0 +1,11 @@
+package dev.pollito.springbootstartertemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterTemplateApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
